96deca9ae43903970c325063a57a4c2c731e1add
ValeryLangolf <lvv.89@rambler.ru>
2025-05-17 22:37:40 +0300

    +

--- Assets/Scripts/Navigation/Waypoint.cs
+++ Assets/Scripts/Navigation/Waypoint.cs
@@ -1,8 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PassengerCategory
+{
+    Economy,
+    Standard,
+    Comfort,
+    Business,
+    Luxury,
+    VIP,
+    PetTransport,
+    WithChildSeat
+}
+
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField, Range(0, 1)] private float _economy;
+    [SerializeField, Range(0, 1)] private float _standard;
+    [SerializeField, Range(0, 1)] private float _comfort;
+    [SerializeField, Range(0, 1)] private float _business;
+    [SerializeField, Range(0, 1)] private float _luxury;
+    [SerializeField, Range(0, 1)] private float _vip;
+    [SerializeField, Range(0, 1)] private float _petTransport;
+    [SerializeField, Range(0, 1)] private float _withChildSeat;
+
     private readonly List<Waypoint> _connectedWaypoints = new();
 
     public List<Waypoint> ConnectedPoints => _connectedWaypoints;
